30fa9a0fb08bf3d02b6c99e1c139f7d040546ac3
Armando Monreal <acalderon.root@gmail.com>
2023-09-22 14:17:01 -0600

    Fix method for getWords

--- src/index.ts
+++ src/index.ts
@@ -6,33 +6,21 @@ async function getChallengeToken() {
     return (await axios.get(`${url}/token`)).data.token;
 }
 
-async function getWords() {
+async function getWords(token: string) {
     const words: string[] = [];
-    const MAX_CONCURRENT_REQUESTS = 8;
-    const token = await getChallengeToken();
+    let index = 0;
 
     while (true) {
         try {
-            const requests = [];
+            const response = await axios.get(`${url}/word`, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            });
 
-            for (let i = 0; i < MAX_CONCURRENT_REQUESTS; i++) {
-                const request = await axios.get(`${url}/word`, {
-                    headers: {
-                        Authorization: `Bearer ${token}`,
-                    },
-                });
+            words.push(response.data.word);
 
-                requests.push(request);
-            }
-
-            const responses = await Promise.allSettled(requests);
-
-            for (const response of responses) {
-
-                if (response.status == 'fulfilled') {
-                    words.push(response.value.data.word);
-                }
-            }
+            index++;
         } catch (error: any) {
             if (error.response.status === 404) {
                 break;
@@ -43,14 +31,14 @@ async function getWords() {
     return words;
 }
 
-async function sortAlphabeticalIgnoredTheFirstLetter() {
-    const words: string[] = await getWords();
+async function sortAlphabeticalIgnoredTheFirstLetter(token: string) {
+    const words: string[] = await getWords(token);
 
     return words.sort((a, b) => a.slice(1).localeCompare(b.slice(1)));
 }
 
-async function joinWords() {
-    return (await sortAlphabeticalIgnoredTheFirstLetter()).join('');
+async function joinWords(token: string) {
+    return (await sortAlphabeticalIgnoredTheFirstLetter(token)).join('');
 }
 
 function fibonacciBinet(number: number): number {
@@ -60,8 +48,8 @@ function fibonacciBinet(number: number): number {
     return Math.round((Math.pow(phi, number) - Math.pow(psi, number)) / Math.sqrt(5));
 }
 
-async function createAccessKey() {
-    const initialString = (await joinWords());
+async function createAccessKey(token: string) {
+    const initialString = (await joinWords(token));
     const sequenceLength = initialString.length;
     const fibonacciSequence = [];
 
@@ -78,9 +66,8 @@ async function createAccessKey() {
     return fibonacciSequence.map(index => initialString[index]).join('');
 }
 
-async function sendContactInformation() {
-    const accessKey = await createAccessKey();
-    const token = await getChallengeToken();
+async function sendContactInformation(token: string) {
+    const accessKey = await createAccessKey(token);
 
     return (await axios.post("https://challenge.bax-dev.com/challenge/apply", {
         name: "Armando Calderon Monreal",
@@ -97,7 +84,9 @@ async function sendContactInformation() {
 
 async function app() {
     try {
-        await sendContactInformation();
+        const token: Promise<string> = await getChallengeToken();
+
+        return await sendContactInformation(await token);
     } catch (error: any) {
         const { message, response } = error;
 
@@ -107,5 +96,5 @@ async function app() {
 
 app()
     .then(response => {
-        console.log(response);
+        console.info(response);
     });
